952a74901d04bf7a97339476411a6f5454ffbfae
Sathishkumar <kfk12688@users.noreply.github.com>
2020-07-31 07:50:18 +0530

    include apex charts in students module basic ui for students module

--- js/app/router.js
+++ js/app/router.js
@@ -12,13 +12,176 @@ function loadHTML(url, id) {
   req.onload = () => {
     if (req.status != 200) console.log(req.statusText);
     $id(id).innerHTML = req.responseText;
+    if (url === "./views/studentsProfileList.html") {
+      loadScript();
+    }
   };
+}
+
+function loadScript() {
+  var options = {
+    series: [
+      {
+        name: "2017-2021",
+        data: [44, 55, 57, 56, 61],
+      },
+      {
+        name: "2018-2022",
+        data: [76, 85, 101, 98, 87],
+      },
+      {
+        name: "2019-2023",
+        data: [35, 41, 36, 26, 45],
+      },
+      {
+        name: "2020-2024",
+        data: [35, 41, 36, 26, 45],
+      },
+    ],
+    chart: {
+      type: "bar",
+      height: 350,
+    },
+    plotOptions: {
+      bar: {
+        horizontal: false,
+        columnWidth: "55%",
+        endingShape: "rounded",
+      },
+    },
+    dataLabels: {
+      enabled: false,
+    },
+    stroke: {
+      show: true,
+      width: 2,
+      colors: ["transparent"],
+    },
+    xaxis: {
+      categories: ["CSE", "EEE", "ECE", "IT", "MECH"],
+    },
+    yaxis: {
+      title: {
+        text: "Profile Counts",
+      },
+    },
+    fill: {
+      opacity: 1,
+    },
+    colors: ["#ed5565", "#23c6c8", "#1c84c6", "#f8ac59"],
+    tooltip: {
+      y: {
+        formatter: function (val) {
+          return val + " Profiles";
+        },
+      },
+    },
+  };
+  var options2 = {
+    series: [
+      {
+        name: "Total",
+        data: [44, 55, 57, 56, 61],
+      },
+    ],
+    chart: {
+      type: "bar",
+      height: 350,
+    },
+    plotOptions: {
+      bar: {
+        horizontal: false,
+        columnWidth: "55%",
+        endingShape: "rounded",
+      },
+    },
+    dataLabels: {
+      enabled: false,
+    },
+    stroke: {
+      show: true,
+      width: 2,
+      colors: ["transparent"],
+    },
+    xaxis: {
+      categories: ["CSE", "EEE", "ECE", "IT", "MECH"],
+    },
+    yaxis: {
+      title: {
+        text: "Total Profiles Based on Departments",
+      },
+    },
+    fill: {
+      opacity: 1,
+    },
+    colors: ["#1ab394"],
+    tooltip: {
+      y: {
+        formatter: function (val) {
+          return val + " Profiles";
+        },
+      },
+    },
+  };
+  var options3 = {
+    series: [
+      {
+        name: "Total",
+        data: [21, 22, 10, 28],
+      },
+    ],
+    chart: {
+      height: 350,
+      type: "bar",
+    },
+    colors: ["#ed5565", "#23c6c8", "#1c84c6", "#f8ac59"],
+    plotOptions: {
+      bar: {
+        distributed: true,
+        horizontal: false,
+        columnWidth: "55%",
+        endingShape: "rounded",
+      },
+    },
+    dataLabels: {
+      enabled: false,
+    },
+    legend: {
+      show: false,
+    },
+    xaxis: {
+      categories: ["2017-2021", "2018-2022", "2019-2023", "2020-2024"],
+      labels: {
+        style: {
+          colors: ["#ed5565", "#23c6c8", "#1c84c6", "#f8ac59"],
+          fontSize: "12px",
+        },
+      },
+    },
+    tooltip: {
+      y: {
+        formatter: function (val) {
+          return val + " Profiles";
+        },
+      },
+    },
+  };
+
+  var chart = new ApexCharts(document.querySelector("#chart"), options);
+  chart.render();
+  var chart2 = new ApexCharts(document.querySelector("#chart2"), options2);
+  chart2.render();
+  var chart3 = new ApexCharts(document.querySelector("#chart3"), options3);
+  chart3.render();
 }
 
 // use #! to hash
 router = new Navigo(null, true, "#!");
 router.on({
   // 'app' is the id of the div element inside which we render the HTML
+  StudentsProfileList: () => {
+    loadHTML("./views/studentsProfileList.html", "app");
+  },
   StudentsProfiles: () => {
     loadHTML("./views/studentsProfiles.html", "app");
   },
